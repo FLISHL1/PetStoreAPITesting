7db8ede2b6a59c81ffc427c29dce6fb7ce6db330
Kirill Konopskii <kirill15022005@gmail.com>
2024-11-19 12:17:07 +0300

    init

---
+++ src/main/java/ru/flish1/testtaskpetshop/TestTaskPetShopApplication.java
@@ -0,0 +1,13 @@
+package ru.flish1.testtaskpetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskPetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskPetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/flish1/testtaskpetshop/TestTaskPetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flish1.testtaskpetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskPetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
